930c60470f9bfe6372a16b55d702a67ec2aa1052
Michal-Kopik <michalkop.t9l@gmail.com>
2024-03-12 11:30:12 +0100

    add GetAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("Hello,  Git!");
 Console.WriteLine("Hello,  pjatk!");
 Console.WriteLine("Hello,  apbd!");
 
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
